5ad5b6e378698a2aedc0da1663c0383724aa190d
Victor <gaeshi@gmail.com>
2021-03-13 17:17:05 +0900

    my solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,16 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  showDetails = false;
+  logEntries = [];
+
+  get buttonLabel() {
+    return this.showDetails ? "Hide Details" : "Display Details";
+  }
+
+  onToggleDisplayDetails() {
+    this.showDetails = !this.showDetails;
+    this.logEntries.push(`${new Date()} ${this.buttonLabel} button clicked.`);
+  }
+}
